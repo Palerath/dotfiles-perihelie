8c754fb8b7cf994c31d4e7024518c302dece41ac
perihelie <archibaldmk4@gmail.com>
2025-08-04 19:46:06 +0200

    created perihelie

---
+++ modules/devel.nix
@@ -0,0 +1,19 @@
+{pkgs, ... }:
+{
+   home.packages = with pkgs; [
+      rustup
+      openssl
+      pkg-config
+      sqlite
+   ];
+
+   services.ollama = {
+      enable = false;
+      acceleration = "cuda";
+   };
+
+   home.sessionVariables = {
+      PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+      RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+   };
+}
